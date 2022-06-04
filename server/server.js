d3af5fa824661116045e41c101f073b207b9455b
Andrey Filip <andreyfilip@MacBook-Pro-Andrey.local>
2022-06-05 00:11:46 +0200

    Task1

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
-import express from 'express'
+import express, { response } from 'express'
 import path from 'path'
 import cors from 'cors'
 import sockjs from 'sockjs'
 import { renderToStaticNodeStream } from 'react-dom/server'
 import React from 'react'
+import axios from 'axios'
 
 import cookieParser from 'cookie-parser'
 import config from './config'
 import Html from '../client/html'
+
+const { readFile, writeFile, unlink } = require('fs').promises
 
 require('colors')
 
@@ -24,6 +27,7 @@ let connections = []
 const port = process.env.PORT || 8090
 const server = express()
 
+
 const middleware = [
   cors(),
   express.static(path.resolve(__dirname, '../dist/assets')),
@@ -33,6 +37,104 @@ const middleware = [
 ]
 
 middleware.forEach((it) => server.use(it))
+
+const globalUrl = 'https://jsonplaceholder.typicode.com/users'
+const globalPatch = `${__dirname}/data/users.json`
+
+const getData = (url) => {
+  const usersList = axios(url)
+   .then(({data}) => {
+     return data
+   })
+   .catch((err) => {
+     console.log(err)
+     return []
+   })
+   return usersList
+}
+
+server.get('/api/v1/users', async (req, res) => {
+  const usersList = await readFile(globalPatch, 'utf-8')
+  .then((usersData) => {
+    return JSON.parse(usersData)
+  })
+  .catch (async (err) => {
+    console.log (err)
+    const reciviedData = await getData(globalUrl)
+    await writeFile(globalPatch, JSON.stringify(reciviedData), 'utf-8')
+    return reciviedData
+   })
+   res.json(usersList)
+})
+
+const writeNewFile = (finalArray) => {
+  return writeFile(globalPatch, JSON.stringify(finalArray), 'utf-8')
+}
+
+server.delete('/api/v1/users/', (req, res) => {
+  unlink(globalPatch)
+    .then(() => {
+      res.json({ status: 'File deleted'})
+    })
+    .catch((err) => {
+      console.log('Error: ', err)
+      res.json({ status: 'No file' })
+    })
+  })
+
+  server.post('/api/v1/users', async (req, res) => {
+    const resposne = await readFile(globalPatch, 'utf-8')
+      .then(async (str) => {
+        const parsedString = JSON.parse(str)
+        const lastId = parsedString[parsedString.length - 1].id + 1
+        await writeNewFile([...parsedString, { ... req.body, id: lastId}])
+        return { status: 'success', id: lastId }
+      })
+      .catch(async (err) => {
+        console.log(err)
+        await writeFile(globalPatch, JSON.stringify([{ ...req.body, id:1 }]), 'utf-8')
+        return {status: 'success', id: 1}
+      })
+      res.json(response)
+    })
+
+
+    server.delete('/api/v1/users/:userId', async (req, res) => {
+      const response1 = await readFile(globalPatch, 'utf-8')
+        .then(async (str) => {
+          const parsedString = JSON.parse(str)
+          const filteredUsers = parsedString.filter((user) => {
+            return +req.params.userId !== user.id
+          })
+          await writeNewFile(filteredUsers)
+          return { status: 'success', id: +req.params.userId }
+        })
+        .catch(() => {
+          return { status: 'no file exist', id: +req.params.userId }
+        })
+      res.json(response1)
+    })
+
+
+     server.patch('/api/v1/users/:userId', async (req, res) => {
+       const { userId } = req.params
+       const updatedUser = {...req.body, i: +userId}
+       const response2 = await readFile(globalPatch, 'utf-8')
+         .then(async (str) => {
+           const parsedString = JSON.parse(str)
+           const updatedlist = parsedString.map((obj) => {
+             return obj.id === +userId ? {...obj, ...updatedUser} : obj
+           })
+           await writeNewFile(updatedlist)
+           return { status: 'success', id: +userId }
+         })
+         .catch(() => {
+           return { status: 'no file exist', id: +userId }
+         })
+       res.json(response2)
+     })
+
+
 
 server.use('/api/', (req, res) => {
   res.status(404)
